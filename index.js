a1d49b4e94c6819a1e4b84df57012f7f03ee8165
WilliamFloydJ <floydfamily71@gmail.com>
2022-03-27 12:05:44 -0600

    Finial Commit

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+const message = document.querySelector("#message");
 const addMovie = (event) => {
   event.preventDefault();
   let inputField = document.querySelector("input");
   const movie = document.createElement("li");
+
   const movieTitle = document.createElement("span");
   movieTitle.textContent = inputField.value;
+  movieTitle.addEventListener("click", crossOffMovie);
   movie.appendChild(movieTitle);
-  document.querySelector("ul").appendChild(movie);
 
+  deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", deleteMovie);
+  movie.appendChild(deleteBtn);
+
+  document.querySelector("ul").appendChild(movie);
   inputField.value = "";
 };
 
+const deleteMovie = (event) => {
+  event.target.parentNode.remove();
+  message.textContent = `${event.target.parentNode.firstChild.textContent} was Deleted!`;
+  revealMessage();
+};
+const crossOffMovie = (event) => {
+  event.target.classList.toggle("checked");
+  if (event.target.classList.contains("checked") === true) {
+    message.textContent = `${event.target.textContent} was watched!`;
+  } else {
+    message.textContent = `${event.target.textContent} was added back to the List!`;
+  }
+  revealMessage();
+};
+const revealMessage = () => {
+  message.classList.remove("hide");
+  setTimeout(() => {
+    message.classList.add("hide");
+  }, 1500);
+};
 document.querySelector("form").addEventListener("submit", addMovie);
